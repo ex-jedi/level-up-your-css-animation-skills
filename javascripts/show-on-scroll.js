f35ab5e3768df3463371b8f04fa9205c096fdb34
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-05-29 18:55:01 +0100

    Relative position on body

--- javascripts/show-on-scroll.js
+++ javascripts/show-on-scroll.js
@@ -1,44 +1,48 @@
 // Detect request animation frame
-var scroll = window.requestAnimationFrame ||
-             window.webkitRequestAnimationFrame ||
-             window.mozRequestAnimationFrame ||
-             window.msRequestAnimationFrame ||
-             window.oRequestAnimationFrame ||
-             // IE Fallback, you can even fallback to onscroll
-             function(callback){ window.setTimeout(callback, 1000/60) };
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+var scroll =
+	window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	window.msRequestAnimationFrame ||
+	window.oRequestAnimationFrame ||
+	// IE Fallback, you can even fallback to onscroll
+	function(callback) {
+		window.setTimeout(callback, 1000 / 60);
+	};
 
+//Target which elements will have reveal class added
+let elementsToShow = document.querySelectorAll(".show-on-scroll");
+
+//Loop through target elements
 function loop() {
+	elementsToShow.forEach(function(element) {
+		if (isElementInViewport(element)) {
+			element.classList.add("is-visible");
+		} else {
+			element.classList.remove("is-visible");
+		}
+	});
 
-    elementsToShow.forEach(function (element) {
-      if (isElementInViewport(element)) {
-        element.classList.add('is-visible');
-      } else {
-        element.classList.remove('is-visible');
-      }
-    });
-
-    scroll(loop);
+	scroll(loop);
 }
 
 // Call the loop for the first time
 loop();
 
-// Helper function from: http://stackoverflow.com/a/7557433/274826
+//Adapted from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
+	var bounding = el.getBoundingClientRect();
+
+	//Set how far the element 'peeks' into the viewport
+	let offsetTop = 0; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
+	let offsetBottom = 50; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
+
+	let offsetFirst = offsetTop - bounding.height;
+	let offsetSecond = bounding.height - offsetBottom;
+	return (
+		bounding.top >= offsetFirst &&
+		bounding.bottom <=
+			(window.innerHeight + offsetSecond ||
+				document.documentElement.clientHeight + offsetSecond)
+	);
 }
