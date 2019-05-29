1559a2b2ecda02df16de82bc1c026799d67f14fa
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-05-29 17:39:07 +0100

    Initial files

--- javascripts/show-on-scroll.js
+++ javascripts/show-on-scroll.js
@@ -1,22 +1,24 @@
 // Detect request animation frame
-var scroll =
-	window.requestAnimationFrame ||
-	// IE Fallback
-	function(callback) {
-		window.setTimeout(callback, 1000 / 60);
-	};
-var elementsToShow = document.querySelectorAll(".show-on-scroll");
+var scroll = window.requestAnimationFrame ||
+             window.webkitRequestAnimationFrame ||
+             window.mozRequestAnimationFrame ||
+             window.msRequestAnimationFrame ||
+             window.oRequestAnimationFrame ||
+             // IE Fallback, you can even fallback to onscroll
+             function(callback){ window.setTimeout(callback, 1000/60) };
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
 function loop() {
-	elementsToShow.forEach(function(element) {
-		if (isElementInViewport(element)) {
-			element.classList.add("is-visible");
-		} else {
-			element.classList.remove("is-visible");
-		}
-	});
 
-	scroll(loop);
+    elementsToShow.forEach(function (element) {
+      if (isElementInViewport(element)) {
+        element.classList.add('is-visible');
+      } else {
+        element.classList.remove('is-visible');
+      }
+    });
+
+    scroll(loop);
 }
 
 // Call the loop for the first time
@@ -24,16 +26,19 @@ loop();
 
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-	var bounding = el.getBoundingClientRect();
-	let offsetTop = 50; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
-	let offsetBottom = 50; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
-
-	let offsetFirst = offsetTop - bounding.height;
-	let offsetSecond = bounding.height - offsetBottom;
-	return (
-		bounding.top >= offsetFirst &&
-		bounding.bottom <=
-			(window.innerHeight + offsetSecond ||
-				document.documentElement.clientHeight + offsetSecond)
-	);
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
 }
