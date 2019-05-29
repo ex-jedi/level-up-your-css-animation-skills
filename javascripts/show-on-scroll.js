b298bd428b5764faf25cabcce4d6dd3e079f267e
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-05-29 08:49:02 +0100

    Adding offset

--- javascripts/show-on-scroll.js
+++ javascripts/show-on-scroll.js
@@ -1,20 +1,22 @@
 // Detect request animation frame
-var scroll = window.requestAnimationFrame ||
-             // IE Fallback
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+var scroll =
+	window.requestAnimationFrame ||
+	// IE Fallback
+	function(callback) {
+		window.setTimeout(callback, 1000 / 60);
+	};
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
 
 function loop() {
+	elementsToShow.forEach(function(element) {
+		if (isElementInViewport(element)) {
+			element.classList.add("is-visible");
+		} else {
+			element.classList.remove("is-visible");
+		}
+	});
 
-  elementsToShow.forEach(function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
-    } else {
-      element.classList.remove('is-visible');
-    }
-  });
-
-  scroll(loop);
+	scroll(loop);
 }
 
 // Call the loop for the first time
@@ -22,19 +24,26 @@ loop();
 
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
+	var bounding = el.getBoundingClientRect();
+	let offsetTop = 50; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
+	let offsetBottom = 50; // Offset in pixels or bounding.height * 0.9 (fraction of elements height)
+
+	let offsetFirst = offsetTop - bounding.height;
+	let offsetSecond = bounding.height - offsetBottom;
+	return (
+		bounding.top >= offsetFirst &&
+		bounding.bottom <=
+			(window.innerHeight + offsetSecond ||
+				document.documentElement.clientHeight + offsetSecond)
+	);
 }
+
+// Get the img heading
+var img = document.querySelector("img");
+
+// Get it's position in the viewport
+var bounding = img.getBoundingClientRect();
+
+// Log the results
+console.log(bounding.height);
+console.log(bounding.height - 50);
